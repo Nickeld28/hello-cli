fa896317d453189f9c27fcc24115274c6005515c
Nickolay Dobrodey <dobrodeynickolay@gmail.com>
2024-07-02 20:34:19 +0300

    Refactor module:
      - Use argparse instead sys.argv
      - Add arguments
      - Add message
      - Add destination and help

--- hello-cli.py
+++ hello-cli.py
@@ -2,21 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import logging
-import sys
+import argparse
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     datefmt='%d-%b-%y %H:%M:%S',
-    level=logging.INFO
+    level=logging.DEBUG
 )
 
 
 def main() -> None:
-    if len(sys.argv) > 1:
-        name = sys.argv[1]
+    parser = argparse.ArgumentParser(
+        prog='hello-cli',
+        usage='%(prog)s [options] [NAME]',
+        description='This program prints "Hello [NAME]".',
+        epilog='epilog'
+    )
+
+    parser.add_argument('name', default='World', help="name of whom to greet", type=str)
+    parser.add_argument('-v', '--verbose', action='store_true', help='enable verbose output')
+    parser.add_argument('-d', '--debug', action='store_true', help='enable debug output')
+
+    args = parser.parse_args()
+    hello_message = f'Hello {args.name}!'
+
+    if args.debug:
+        logging.debug(args)
+    if args.verbose:
+        logging.info(hello_message)
     else:
-        name = 'World'
-    logging.info(f'Hello {name}!')
+        print(hello_message)
 
 
 if __name__ == '__main__':
